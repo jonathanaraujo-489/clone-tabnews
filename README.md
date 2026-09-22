11010011619066c7530c097e63a25d51594d1983
Jonathan Araújo <jonathan@alissonjoias.com.br>
2026-09-22 10:14:48 +0000

    adicionar script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do clone do TabNews, desenvolvido durante o Curso.dev para praticar desenvolvimento web end-to-end e melhores práticas de engenharia de software.
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,15 @@ function Home() {
   return (
     <div>
       <h1>Formula do Sucesso.</h1>
-     <p>🙏 Deus + 📚 Estudo + 💪 Trabalho Duro - 🤡 Trabalho Burro = 🚀 Sucesso!!</p> 
+      <p>
+        🙏 Deus + 📚 Estudo + 💪 Trabalho Duro - 🤡 Trabalho Burro = 🚀
+        Sucesso!!
+      </p>
     </div>
   );
 }
 
 export default Home;
+function teste() {
+  console.log("indentacao errada");
+}
